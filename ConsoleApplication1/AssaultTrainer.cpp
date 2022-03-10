de23c1b8a57df7f2247aafe98f7396f93f614366
John Kennedy <johnkennedy2019@knights.ucf.edu>
2022-03-09 23:54:33 -0500

    More reorganization of files, made menu for user, removed unneccessary console logs

--- ConsoleApplication1/AssaultTrainer.cpp
+++ ConsoleApplication1/AssaultTrainer.cpp
@@ -5,8 +5,8 @@
 #include <Windows.h>
 #include <iostream>
 #include <string>
-#include "logInfo.h"
-#include "PlayerInfo.h"
+#include "./headers/logInfo.h"
+#include "./headers/PlayerInfo.h"
 
 
 
@@ -16,19 +16,17 @@ int main()
     DWORD ProcessID;
     HWND hwnd = FindWindowA(0, ("AssaultCube")); // We will be targetting AssaultCube
     GetWindowThreadProcessId(hwnd, &ProcessID);  // Retrieving the Process ID of assaultCube
-    if (hwnd) {
-        logOut(ProcessID);
-    }
-    else {
-        logOut("unable to get processID");
-        
+    if (!hwnd) {
+        logOut("unable to find game");
+
         return EXIT_FAILURE;
     }
-    //
+
     HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessID); //Making a handle to the process
     
     uintptr_t playerStruct = offsets.base; //0x400000 + 0x0017B0B8 -- 40000 is the best address and 0017b0b8 is the offset to the player struct
     int localPlayer = 0;
+    
     uintptr_t ammoOffset = offsets.ammo_ar;
     uintptr_t ammoAddress = playerStruct + ammoOffset;
     int ammo = 0;
@@ -39,7 +37,7 @@ int main()
     
     //Follow the pointer to the playerStruct
     BOOL rpmReturn = ReadProcessMemory(hProcess, (LPCVOID)playerStruct, &localPlayer, sizeof(localPlayer), NULL);
-    std::cout << localPlayer << std::endl;
+
 
     //Access the ammo from the pointer result + the offset
     ReadProcessMemory(hProcess, (LPCVOID)(localPlayer + ammoOffset), &ammo, sizeof(ammo), NULL);
@@ -48,14 +46,37 @@ int main()
         return EXIT_FAILURE;
     }
     //Print out the ammo
-    logOut("Player's carbine ammo: " + std::to_string(ammo));
+    //logOut("Player's carbine ammo: " + std::to_string(ammo));
 
     int newAmmo = 20; //ammo value to be written to the memory
     WriteProcessMemory(hProcess, (LPVOID)(localPlayer + ammoOffset), &newAmmo, sizeof(ammo), NULL); //set the players ammo;
-    logOut("Player's carbine ammo has been set to: " + std::to_string(newAmmo));
+    //logOut("Player's carbine ammo has been set to: " + std::to_string(newAmmo));
 
+     
+    int userInput = 1;
+
+    while (userInput != 0) {
+        logOut("What do you want to do\n0: Quit \n1: Display Carbine ammo \n2: Change Carbine ammo");
+        std::cin >> userInput;
+        switch (userInput) {
+            case 1:
+                ReadProcessMemory(hProcess, (LPCVOID)(localPlayer + ammoOffset), &ammo, sizeof(ammo), NULL);
+                logOut("Current Carbine Ammo: " + std::to_string(ammo));
+                logOut("");
+                break;
+            case 2:
+                logOut("Enter The New Ammo Count");
+                std::cin >> newAmmo;
+                WriteProcessMemory(hProcess, (LPVOID)(localPlayer + ammoOffset), &newAmmo, sizeof(newAmmo), NULL);
+                logOut("");
+                break;
+            default:
+                logOut("Quitting trainer");
+                userInput = 0;
+                break;
+        }
+    }
     CloseHandle(hProcess); //close the handle to the game
-
     return EXIT_SUCCESS;
 }
 
--- ConsoleApplication1/headers/logging.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-
-void log(std::String s);
-
-void log(std::String s) {
-	std::cout << s << std::endl;
-}
